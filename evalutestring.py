7583c52af9e3c9a97e94b93cdd92e4b41f4018e9
phalleinin <156156002+phalleinin@users.noreply.github.com>
2024-01-26 08:52:18 +0700

    Update evalutestring.py

--- evalutestring.py
+++ evalutestring.py
@@ -1,16 +1,16 @@
+import string
+# Given a string indicating a range of letters
+input_str = input ("Enter a range of letters e.g., a-z: ") # alphabet = abcdefghijklmnopqrstuvwxyz
 
-# Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
-# Note that if the range is given in capital letters, return the string in capitals also!
+# Split the input into the start and end characters of the range (e.g., "a" and "z")
+start_letter, end_letter = input_str.split('-')
 
-# Examples
-# "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
-# "h-o" ➞ "hijklmno"
-# "Q-Z" ➞ "QRSTUVWXYZ"
-# "J-J" ➞ "J"
-# Notes a hyphen will separate the two letters in the string.
+# Convert the start_char and end_char to their ASCII values 
+start_ascii = ord(start_letter)
+end_ascii = ord(end_letter)
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
+result_string = ''.join(chr(i) for i in range(ord(start_letter), ord(end_letter)+ 1 ))
 
-user_range = input("Enter a range of letters (e.g., a-z): ")
+# Display the result
+print ("A range letter is", result_string)
 
-print(result_string)
